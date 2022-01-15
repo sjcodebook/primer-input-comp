2181ea84cd99273c28179872467457305b28bfef
Sanmati Kumar Jain <sjmac@Sanmatis-MacBook-Air.local>
2022-01-16 02:58:44 +0530

    Init

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Testing Input Components', () => {
+  it('should render correctly App Component', () => {
+    render(<App />)
+    const mainDiv = screen.getByTestId('main-div')
+    expect(mainDiv).not.toBeNull()
+  })
+
+  it('should render correctly Berth id', () => {
+    render(<App />)
+    const berthLabel = screen.getByTestId('berth-label')
+    expect(berthLabel).toHaveTextContent('Berth Id')
+  })
+
+  it('should render correctly remark', () => {
+    render(<App />)
+    const remarkLabel = screen.getByTestId('remark-label')
+    expect(remarkLabel).toHaveTextContent('Remark')
+  })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { InputText } from 'primereact/inputtext'
+import { InputTextarea } from 'primereact/inputtextarea'
 
-function App() {
+// interface InputTextDemoProps {
+//   label: string
+//   name?: string
+// }
+
+const InputTextDemo = () => {
+  const [text1, setText1] = useState<string>('')
+  const [text2, setText2] = useState<string>('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div data-testid='main-div'>
+      <div className='card'>
+        <h5 data-testid='berth-label' style={{ color: 'red' }}>
+          Berth Id
+        </h5>
+        <InputText
+          value={text1}
+          placeholder='please type'
+          onChange={(e: any) => setText1(e.target.value)}
+          style={{
+            borderRadius: 0,
+            borderBottom: '2px solid grey',
+            borderTop: 'none',
+            borderRight: 'none',
+            borderLeft: 'none',
+          }}
+        />
+        <h5 data-testid='remark-label'>Remark</h5>
+        <InputTextarea
+          value={text2}
+          placeholder='please typesd'
+          onChange={(e: any) => setText2(e.target.value)}
+          rows={20}
+          cols={30}
+        />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default InputTextDemo
+
+// import React, { Component } from 'react'
+
+// export class InputTextDemo extends Component {
+//   state = {
+//     text1: '',
+//     text2: '',
+//   }
+
+//   render() {
+//     return (
+//       <div data-testid='main-div'>
+//         <div className='card'>
+//           <h5 data-testid='berth-label' style={{ color: 'red' }}>
+//             Berth Id
+//           </h5>
+//           <InputText
+//             value={this.state.text1}
+//             placeholder='please type'
+//             onChange={(e: any) =>
+//               this.setState({
+//                 text1: e.target.value,
+//               })
+//             }
+//             style={{
+//               borderRadius: 0,
+//               borderBottom: '2px solid grey',
+//               borderTop: 'none',
+//               borderRight: 'none',
+//               borderLeft: 'none',
+//             }}
+//           />
+//           <h5 data-testid='remark-label'>Remark</h5>
+//           <InputTextarea
+//             value={this.state.text2}
+//             placeholder='please typesd'
+//             onChange={(e: any) =>
+//               this.setState({
+//                 text2: e.target.value,
+//               })
+//             }
+//             rows={20}
+//             cols={30}
+//           />
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+// export default InputTextDemo
